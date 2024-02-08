06417158b4de3f2c716c94c6e0414207f10d8724
rafaelanggawijaya <124117040+rafaelanggawijaya@users.noreply.github.com>
2024-02-09 11:10:16 +1300

    finished 3

--- VolumeOfConcrete.py
+++ VolumeOfConcrete.py
@@ -1,21 +1,27 @@
 building_type = ""
-while building_type != "X" or building_type != "x": 
+while building_type != "0":
     while True:
-            building_type = input("What building type (residential (r) or commercial (c)) exit (X): ")
-            if building_type == "residential" or building_type == "r":
-                building_type == float(0.25)
-                break
-            elif building_type == "commercial" or building_type == "c":
-                building_type == float(0.5)
-                break
-            else:
-                input("Please enter a valid answer (r or c)")
-
-    length = float(input("What length is the building?: "))
-    width = float(input("What width is the building?: "))
-    volume = length*width*building_type
-    print(f"The volume of concrete required for a slab with a\nlength of {length}m\na width of {width}m\nand a depth of {building_type}m is {volume} cubic metres")
-    
-            
-
-    
+        building_type = input("What building type (residential (r) or "
+                              "commercial (c)) exit (X): ")
+        if building_type == "residential" or building_type == "r":
+            building_type = 0.25
+            break
+        elif building_type == "commercial" or building_type == "c":
+            building_type = 0.5
+            break
+        elif building_type == "X" or "x":
+            print("Program ended Goodbye")
+            building_type = "0"
+            break
+        else:
+            print("Please enter a valid answer (r or c)")
+    if building_type == "0":
+        building_type = "0"
+        break
+    else:
+        length = float(input("What length is the building?: "))
+        width = float(input("What width is the building?: "))
+        volume = float(length * width * building_type)
+        print(f"The volume of concrete required for a slab with a\n"
+              f"length of {length}m\na width of {width}m\nand a depth of "
+              f"{building_type}m \nis {volume} cubic metres")
